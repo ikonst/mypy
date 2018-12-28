88a94aae2ec262b7712b5d38f15f2a4d54fb6a3d
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2018-12-28 13:44:42 -0800

    Move matching logic to get_function_hook

--- mypy/plugins/pynamodb.py
+++ mypy/plugins/pynamodb.py
@@ -1,42 +1,49 @@
 from typing import Callable, Optional, Type
 
 import mypy.types
-from mypy.nodes import NameExpr, CallExpr, TypeInfo
-from mypy.plugin import Plugin, FunctionContext
+from mypy.nodes import NameExpr, TypeInfo
+from mypy.plugin import FunctionContext, Plugin
 
 ATTR_FULL_NAME = 'pynamodb.attributes.Attribute'
+NULL_ATTR_FULL_NAME = 'pynamodb.attributes._NullableAttribute'
 
 
 class PynamodbPlugin(Plugin):
     def get_function_hook(self, fullname: str) -> Optional[Callable[[FunctionContext], mypy.types.Type]]:
-        return _function_hook_callback
+        node = self.lookup_fully_qualified(fullname)
+        if (
+            node and
+            isinstance(node.node, TypeInfo) and
+            node.node.has_base(ATTR_FULL_NAME) and
+            # TODO: any better way to tell apart instantiation from a function call?
+            fullname == node.node.fullname()
+        ):
+            return _function_hook_callback
 
 
 def _function_hook_callback(ctx: FunctionContext) -> mypy.types.Type:
-    if (
-        not isinstance(ctx.default_return_type, mypy.types.Instance) or
-        not ctx.default_return_type.type.has_base(ATTR_FULL_NAME) or
-        # TODO: any better way to tell apart a construction from other functions?
-        not isinstance(ctx.context, CallExpr) or
-        not isinstance(ctx.context.callee, NameExpr) or
-        not isinstance(ctx.default_return_type.type, TypeInfo) or
-        not ctx.context.callee.fullname == ctx.default_return_type.type.fullname()
-    ):
-        return ctx.default_return_type
-
     attr_type = ctx.default_return_type
-    base_types = [base for base in attr_type.type.bases
-                  if base.type.fullname() == 'pynamodb.attributes.Attribute']
-    if not base_types:
+    base_type = next(base for base in attr_type.type.bases
+                     if base.type.fullname() == ATTR_FULL_NAME)
+    try:
+        (underlying_type,) = base_type.args
+    except ValueError:
+        ctx.api.fail(f'Unexpected number of type arguments to {ATTR_FULL_NAME}', ctx.context)
         return ctx.default_return_type
-    base_type = base_types[0]
-    underlying_type = base_type.args[0]
 
-    # If initializer gets named arg null=True,
-    # wrap in _NullableAttribute to make the underlying type optional
-    for arg_name, arg_expr in zip(ctx.context.arg_names, ctx.context.args):
+    # If initializer is passed null=True, wrap in _NullableAttribute
+    # to make the underlying type optional
+    for arg_name, arg_exprs in zip(ctx.callee_arg_names, ctx.args):
+        if arg_name != 'null' or not arg_exprs:
+            continue
+        try:
+            (arg_expr,) = arg_exprs
+        except ValueError:
+            ctx.api.fail("Unexpected number of expressions in 'null' argument", ctx.context)
+            continue
+
         if isinstance(arg_expr, NameExpr) and arg_expr.fullname == 'builtins.True':
-            return ctx.api.named_generic_type('pynamodb.attributes._NullableAttribute', [
+            return ctx.api.named_generic_type(NULL_ATTR_FULL_NAME, [
                 attr_type,
                 underlying_type,
             ])
